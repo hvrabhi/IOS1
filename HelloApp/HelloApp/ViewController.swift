199f29609c72ddbda86e9d415b530ca52a854ea2
Abhilash Reddy Malipatlola <33493095+hvrabhi@users.noreply.github.com>
2022-01-20 12:01:04 -0600

    Add files via upload

---
+++ HelloApp/HelloApp/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  HelloApp
+//
+//  Created by Malipatlola,Abhilash Reddy on 1/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var nameOutlet: UITextField!
+    
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func buttonClicked(_ sender: UIButton) {
+        //Read the data from the text box and store it in a variable
+        var name = nameOutlet.text!
+        
+        //change the text in display label with this format. Hello, name!
+        
+        displayLabel.text = "Hello, \(name)!"
+        
+    }
+    
+}
+
