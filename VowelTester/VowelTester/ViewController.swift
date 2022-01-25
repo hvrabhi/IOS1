e76b21c5cd9624ff9ce1a5f72b91bfa6e0ba0637
Abhilash Reddy Malipatlola <33493095+hvrabhi@users.noreply.github.com>
2022-01-25 11:59:28 -0600

    Add files via upload

---
+++ VowelTester/VowelTester/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  VowelTester
+//
+//  Created by Malipatlola,Abhilash Reddy on 1/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var textOutlet: UITextField!
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func buttonClick(_ sender: UIButton) {
+        
+        //Read the text
+        var enteredText = textOutlet.text!
+        //Check vowel or not
+        if(enteredText.contains("a") || enteredText.contains("e") || enteredText.contains("i") || enteredText.contains("o") || enteredText.contains("u") ){
+            displayLabel.text = "The text contains vowel"
+        }
+        else{
+            displayLabel.text = "The text doesnot contain vowel"
+        }
+    }
+    
+}
+
